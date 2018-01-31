6d66987c6ba848002685b2ad46fa2ef92dd032a3
Jared <Jared@Littles-MBP.fios-router.home>
2018-01-31 16:06:45 -0500

    added playing rules to the readme

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ConstructorHangman
+
+Play hangman by guessing a letter. If the letter you guessed is in the word, it will appear in the
+underscored hidden word. All letters already guessed will be pushed to the screen so you don't guess the 
+same letter twice.
--- letter.js
+++ letter.js
@@ -1,14 +1,6 @@
 var guess = function(letterGuess){
 	this.letterGuess = letterGuess;
-	// this.wrongGuess = wrongGuess;
-	// this.rightGuess = rightGuess;
-	// this.checkLetter = function () {
-	// 	for(var i = 0; i < this.length; i++) {
-	// 	if (this.guess = word[i]){
-
-	// 		}
-	// 	}
-	// }
+	
 	console.log('letterGuess')
 };
 
